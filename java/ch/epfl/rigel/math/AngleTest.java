ff128c1a89bede32aa4b0668415c260d669847bc
Alexandre Sallinen <alex.sallinen@gmail.com>
2020-02-17 18:07:54 +0100

    Added tests for ClosedIntervals Added tests for AngleTest

---
+++ java/ch/epfl/rigel/math/AngleTest.java
@@ -0,0 +1,36 @@
+package ch.epfl.rigel.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AngleTest {
+
+    @Test
+    void normalizePositive() {
+    }
+
+    @Test
+    void ofArcsec() {
+    }
+
+    @Test
+    void ofDMS() {
+    }
+
+    @Test
+    void ofDeg() {
+    }
+
+    @Test
+    void toDeg() {
+    }
+
+    @Test
+    void ofHr() {
+    }
+
+    @Test
+    void toHr() {
+    }
+}
---
+++ java/ch/epfl/rigel/math/ClosedIntervalTest.java
@@ -0,0 +1,39 @@
+package ch.epfl.rigel.math;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClosedIntervalTest {
+
+    private static ClosedInterval closedInterval;
+
+    @BeforeAll
+    private static void setUp()
+    {
+        closedInterval = ClosedInterval.of(-5, 9);
+    }
+    @Test
+    void clip() {
+        assertEquals(5, closedInterval.clip(5));
+        assertEquals(9, closedInterval.clip(520));
+        assertEquals(-5, closedInterval.clip(-600));
+
+
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("[ -5.00, 9.00 ]", closedInterval.toString());
+    }
+
+    @Test
+    void contains() {
+        assertTrue(closedInterval.contains(4));
+        assertTrue(closedInterval.contains(9));
+        assertFalse(closedInterval.contains(765));
+
+
+    }
+}
