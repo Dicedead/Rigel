0b51ea30ba3beecb3f9f24ee6ca3fbd83a378aa5
Alexandre Sallinen <alex.sallinen@gmail.com>
2020-02-19 13:36:45 +0100

    Commented SphericalCoordinates class

--- java/ch/epfl/rigel/coordinates/SphericalCoordinates.java
+++ java/ch/epfl/rigel/coordinates/SphericalCoordinates.java
@@ -8,16 +8,39 @@ class SphericalCoordinates
     final private double longitude;
     final private double latitude;
 
-
+    /**
+     * Constructor for SphericalCoordinates
+     * @param longitude input in radians for longitude
+     * @param latitude input in radians for latitude
+     */
     SphericalCoordinates(double longitude, double latitude)
     {
         this.latitude   = latitude;
         this.longitude  = longitude;
     }
 
+    /**
+     * Getter for longitude in radians
+     * @return longitude in radians
+     */
     double lon() { return longitude; }
+
+    /**
+     * Getter for longitude in degrees
+     * @return longitude in degrees
+     */
     double lonDeg() {return toDeg(longitude);}
+
+    /**
+     * Getter for latitude in radians
+     * @return latitude in radians
+     */
     double lat(){ return latitude; }
+
+    /**
+     * Getter for latitude in degrees
+     * @return latitude in degrees
+     */
     double latDeg(){return toDeg(latitude);}
 
     @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
