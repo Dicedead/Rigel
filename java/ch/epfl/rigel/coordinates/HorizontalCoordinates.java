1bedcfcf3460f2dbc962ca5993656fad1192b44c
Dicedead <dicedead@gmail.com>
2020-02-19 17:49:02 +0100

    suppression package en trop + presque fini HorizontalCoordinates

--- java/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
+++ java/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
@@ -43,7 +43,7 @@ public final class HorizontalCoordinates extends SphericalCoordinates {
     }
 
     public String azOctantName(String n, String e, String s, String w) {
-
+        
     }
 
     public double alt() {
