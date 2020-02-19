5f98e08e4c19837a70f2c7452cb415f28e61319f
Alexandre Sallinen <alex.sallinen@gmail.com>
2020-02-19 14:03:22 +0100

    Finished GeographicCoordinates and commented

--- java/ch/epfl/rigel/coordinates/GeographicCoordinates.java
+++ java/ch/epfl/rigel/coordinates/GeographicCoordinates.java
@@ -3,6 +3,10 @@ import ch.epfl.rigel.Preconditions;
 import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.RightOpenInterval;
 
+import java.util.Locale;
+
+import static ch.epfl.rigel.math.Angle.toDeg;
+
 
 public final class GeographicCoordinates extends SphericalCoordinates
 {
@@ -22,24 +26,63 @@ public final class GeographicCoordinates extends SphericalCoordinates
      */
     GeographicCoordinates ofDeg(double lonDeg, double latDeg)
     {
-        Preconditions.checkInInterval(RightOpenInterval.symmetric(360), lonDeg);
-        Preconditions.checkInInterval(RightOpenInterval.symmetric(360), latDeg);
 
-        return new GeographicCoordinates(Angle.ofDeg(lonDeg), Angle.ofDeg(latDeg));
+        return new GeographicCoordinates(
+                Angle.ofDeg(Preconditions.checkInInterval(RightOpenInterval.symmetric(360), lonDeg)),
+                Angle.ofDeg(Preconditions.checkInInterval(RightOpenInterval.symmetric(360), latDeg)
+                ));
 
     }
 
     /**
      * Checks that the passed angle is a valid longitude in degrees
-     * @param lonDeg
-     * @return
+     * @param lonDeg angle input in degrees
+     * @return true if it is between -180° and 180°
      */
     static  public  boolean isValidLonDeg(double lonDeg)
     {
-
+        return RightOpenInterval.symmetric(360).contains(lonDeg);
     }
+    /**
+     * Checks that the passed angle is a valid latitude in degrees
+     * @param latDeg angle input in degrees
+     * @return true if it is between -180° and 180°
+     */
     boolean isValidLatDeg(double latDeg)
     {
+        return RightOpenInterval.symmetric(360).contains(latDeg);
+    }
 
+
+    /**
+     * Getter for longitude in radians
+     * @return longitude in radians
+     */
+    public double lon() { return super.lon(); }
+
+    /**
+     * Getter for longitude in degrees
+     * @return longitude in degrees
+     */
+    public double lonDeg() {return super.lonDeg();}
+
+    /**
+     * Getter for latitude in radians
+     * @return latitude in radians
+     */
+    public double lat(){ return super.lat(); }
+
+    /**
+     * Getter for latitude in degrees
+     * @return latitude in degrees
+     */
+    public double latDeg(){return super.latDeg();}
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT,
+                                "(lon=%.4f°, lat=%4f°)",
+                            lonDeg(),
+                            latDeg());
     }
 }
