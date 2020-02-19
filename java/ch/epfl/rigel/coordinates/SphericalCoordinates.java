64eb434d6a6b7a88e140d4a84bd99b3dad4e4afa
Alexandre Sallinen <alex.sallinen@gmail.com>
2020-02-19 13:33:40 +0100

    SphericalCoordinates class completed Lacks documentation

--- java/ch/epfl/rigel/coordinates/SphericalCoordinates.java
+++ java/ch/epfl/rigel/coordinates/SphericalCoordinates.java
@@ -1,4 +1,37 @@
 package ch.epfl.rigel.coordinates;
 
-public class SphericalCoordinates {
+import static ch.epfl.rigel.math.Angle.toDeg;
+
+class SphericalCoordinates
+{
+
+    final private double longitude;
+    final private double latitude;
+
+
+    SphericalCoordinates(double longitude, double latitude)
+    {
+        this.latitude   = latitude;
+        this.longitude  = longitude;
+    }
+
+    double lon() { return longitude; }
+    double lonDeg() {return toDeg(longitude);}
+    double lat(){ return latitude; }
+    double latDeg(){return toDeg(latitude);}
+
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    @Override
+    public final boolean equals(Object o) {
+        System.err.println("Fatal error : tried to test equality between intervals but double precision does not \n" +
+                "allows it.");
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final int hashCode() {
+        System.err.println("Fatal error : tried to test equality between intervals but double precision does not \n" +
+                "allows it.");
+        throw new UnsupportedOperationException();
+    }
 }
