6e135d103244a357b071811ae2fdb7cb4dca3115
Dicedead <dicedead@gmail.com>
2020-03-02 14:49:11 +0100

    CartesCoords toString, hash, equals

--- src/ch/epfl/rigel/coordinates/CartesianCoordinates.java
+++ src/ch/epfl/rigel/coordinates/CartesianCoordinates.java
@@ -1,4 +1,7 @@
 package ch.epfl.rigel.coordinates;
+
+import java.util.Locale;
+
 /**
  * Cartesian coordinates representaation
  *
@@ -6,6 +9,8 @@ package ch.epfl.rigel.coordinates;
  * @author Salim Najib (310003)
  */
 public final class CartesianCoordinates {
+
+    private final double x, y;
 
     private CartesianCoordinates(double x, double y) {
         this.x = x;
@@ -18,7 +23,7 @@ public final class CartesianCoordinates {
      * @param y coordinate
      * @return a cartesian coordinates
      */
-    static CartesianCoordinates of(double x, double y)
+    public static CartesianCoordinates of(double x, double y)
     {
         return new CartesianCoordinates(x, y);
     }
@@ -39,6 +44,24 @@ public final class CartesianCoordinates {
         return y;
     }
 
-    private final double x, y;
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT,
+                "CartesianCoordinates: x= %.4f; y= %.4f",x,y);
+    }
 
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    @Override
+    public final boolean equals(Object o) {
+        //System.err.println("Fatal error : tried to test equality between intervals but double precision does not \n" +
+        //        "allows it.");
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public final int hashCode() {
+        //System.err.println("Fatal error : tried to test equality between intervals but double precision does not \n" +
+        //        "allows it.");
+        throw new UnsupportedOperationException();
+    }
 }
