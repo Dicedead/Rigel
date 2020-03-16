c47ce6b9fc6fec9f0110be400cf36a82b81c42eb
Dicedead <dicedead@gmail.com>
2020-03-16 14:12:47 +0100

    must commit to pull lol

---
+++ src/ch/epfl/rigel/astronomy/StarCatalogue.java
@@ -0,0 +1,11 @@
+package ch.epfl.rigel.astronomy;
+
+import java.util.List;
+
+public final class StarCatalogue {
+
+    public StarCatalogue(List<Star> stars, List<Asterism> asterisms) {
+
+
+    }
+}
