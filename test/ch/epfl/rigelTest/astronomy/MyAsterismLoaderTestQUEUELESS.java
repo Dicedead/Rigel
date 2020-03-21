9074c82d51b7932e74d8cacc3d8cbc0875ab9afa
Dicedead <dicedead@gmail.com>
2020-03-21 20:28:08 +0100

    queueless asterismLoaderTest

---
+++ test/ch/epfl/rigelTest/astronomy/MyAsterismLoaderTestQUEUELESS.java
@@ -0,0 +1,53 @@
+package ch.epfl.rigelTest.astronomy;
+
+import ch.epfl.rigel.astronomy.Asterism;
+import ch.epfl.rigel.astronomy.AsterismLoader;
+import ch.epfl.rigel.astronomy.HygDatabaseLoader;
+import ch.epfl.rigel.astronomy.Star;
+import ch.epfl.rigel.astronomy.StarCatalogue;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MyAsterismLoaderTestQUEUELESS {
+
+    private static final String HYG_CATALOGUE_NAME =
+            "/hygdata_v3.csv";
+    private static final String AST_CATALOGUE_NAME =
+            "/asterisms.txt";
+
+    @Test
+    void loadWorks() throws IOException {
+        StarCatalogue.Builder builder;
+        Asterism a = null;
+        Star beltegeuse = null;
+        try (InputStream hygStream = getClass()
+                .getResourceAsStream(HYG_CATALOGUE_NAME)) {
+            builder = new StarCatalogue.Builder()
+                    .loadFrom(hygStream, HygDatabaseLoader.INSTANCE);
+            }
+        try (InputStream astStream = getClass()
+                .getResourceAsStream(AST_CATALOGUE_NAME)) {
+            StarCatalogue catalogue = builder
+                    .loadFrom(astStream, AsterismLoader.INSTANCE)
+                    .build();
+            for(Asterism ast : catalogue.asterisms()){
+                for(Star s : ast.stars()){
+                    if(s.name().equalsIgnoreCase("rigel")){
+                        a = ast;
+                    }
+                }
+            }
+            for(Star s : a.stars()){
+                if(s.name().equalsIgnoreCase("Betelgeuse")){
+                    beltegeuse = s;
+                }
+            }
+            assertNotNull(beltegeuse);
+            }
+        }
+
+}
